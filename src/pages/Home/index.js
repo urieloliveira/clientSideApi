4c2f728432d820340f68e16588adbaa42679fc26
Uriel <urielsateles@gmail.com>
2021-03-25 20:20:56 -0300

    feat: ajustes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Container, Content,Card_link, Icon, icon_rigth, Button,Menu, Header, Menu_links, Button_outline, Logo, Menu_Icon_Open,Card, Card_title, Card_description, Hamburguer_menu} from "./styles";
+import {
+  Container,
+  Content,
+  Card_link,
+  Icon,
+  icon_rigth,
+  Button,
+  Menu,
+  Header,
+  Menu_links,
+  Button_outline,
+  Logo,
+  Menu_Icon_Open,
+  Card,
+  Card_title,
+  Card_description,
+  Hamburguer_menu,
+} from "./styles";
 
-function Home() {
+function Home(props) {
   return (
     <Container>
       <Header>
         <Menu>
-          <Logo href="/"></Logo> 
+          <Logo href="/"></Logo>
           <Menu_Icon_Open onclick="toggleMenu(true)"></Menu_Icon_Open>
           <Menu_links>
             <li>Waze</li>
@@ -17,69 +34,97 @@ function Home() {
             <li>Pegue carona</li>
             <li>Mapa ao vivo</li>
             <li>Baixar</li>
-            <li><Button_outline>Entrar</Button_outline></li>
+            <li>
+              <Button_outline onClick={() => props.history.push("/search")}>
+                Entrar
+              </Button_outline>
+            </li>
           </Menu_links>
         </Menu>
-
       </Header>
       <Content>
-
         <Card>
-            <img src="https://kstatic.googleusercontent.com/files/46e435e488928422ac7fee9e6883889260e4dd88205a637156c8e0ea5f46e1990520ef2947f18a28d07435aa70a69b8bc9a6e1b5b299d998477a8974bdbf4317" alt="" class="card-logo"/>
-            <Card_title>Um mundo com menos trânsito é possível</Card_title>
-            <Card_description>Seja uma das +130 milhões de pessoas que ajudam a construí-lo.</Card_description>
-            <Button>Baixar</Button>
-            <Card_link href="">Saiba mais</Card_link>
-            <Icon style={{
+          <img
+            src="https://kstatic.googleusercontent.com/files/46e435e488928422ac7fee9e6883889260e4dd88205a637156c8e0ea5f46e1990520ef2947f18a28d07435aa70a69b8bc9a6e1b5b299d998477a8974bdbf4317"
+            alt=""
+            class="card-logo"
+          />
+          <Card_title>Um mundo com menos trânsito é possível</Card_title>
+          <Card_description>
+            Seja uma das +130 milhões de pessoas que ajudam a construí-lo.
+          </Card_description>
+          <Button>Baixar</Button>
+          <Card_link href="">Saiba mais</Card_link>
+          <Icon
+            style={{
               right: "60px",
-              left: "auto"
-            }}>
-              <img src="https://kstatic.googleusercontent.com/files/9aab8ecf0d18ddbdd0e837e9279110d7f2a6c95c0f54813c6bf0c8603b3748f292928e3305b5ed27a623f1ed8e989c7b0e5fae7b2ea343722c6b42fddb2a8604" alt=""/>
-            </Icon>
+              left: "auto",
+            }}
+          >
+            <img
+              src="https://kstatic.googleusercontent.com/files/9aab8ecf0d18ddbdd0e837e9279110d7f2a6c95c0f54813c6bf0c8603b3748f292928e3305b5ed27a623f1ed8e989c7b0e5fae7b2ea343722c6b42fddb2a8604"
+              alt=""
+            />
+          </Icon>
         </Card>
         <Card>
-              <Card style={{
-                  left: "0",
-                  height: "100%",
-                  position: "absolute",
-                  width:" calc(50% + 40px)",
-                  borderTopLeftRadius: "10px",
-                  borderBottomLeftRadius: "10px",
-                  background: "#FCE354",
-                  clipPath: "polygon(0 0, 70% 0, 100% 100%, 0 100%)"
-              }}></Card>
-              <Card style={{
-                  right: "0",
-                  height: "100%",
-                  position: "absolute",
-                  width: "calc(50% + 40px)",
-                  backgroundImage: `url("https://lh3.googleusercontent.com/QRuzdAHgO26osbICxbUK-1LeEZRZK0a_ggFr0RZNkFRT_Syk4ArnEb2-2eaUUFOrNyoxLZGT8KAl4JAgc7XTd8MYiIDTjy38FQyjAA")`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  borderTopRightRadius: "10px",
-                  borderBottomRightRadius: "10px",
-                  clipPath:"polygon(0 0, 100% 0, 100% 100%, 30% 100%)"
-              }}></Card>
-              <Icon>
-                  <img src="https://kstatic.googleusercontent.com/files/811eae0c24a1c64093bacbc3f717cbda8a6e494d48fa1788017542b8e258050dcf185ced5794936ef634f77d18e3a4e67edde7ebcf725e209bd62f2f489ad033" alt=""/>
-              </Icon>
+          <div
+            style={{
+              left: "0",
+              height: "100%",
+              position: "absolute",
+              width: " calc(50% + 40px)",
+              borderTopLeftRadius: "10px",
+              borderBottomLeftRadius: "10px",
+              background: "#FCE354",
+              clipPath: "polygon(0 0, 70% 0, 100% 100%, 0 100%)",
+            }}
+          />
+          <div
+            style={{
+              right: "0",
+              height: "100%",
+              position: "absolute",
+              width: "calc(50% + 40px)",
+              backgroundImage: `url("https://lh3.googleusercontent.com/QRuzdAHgO26osbICxbUK-1LeEZRZK0a_ggFr0RZNkFRT_Syk4ArnEb2-2eaUUFOrNyoxLZGT8KAl4JAgc7XTd8MYiIDTjy38FQyjAA")`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              borderTopRightRadius: "10px",
+              borderBottomRightRadius: "10px",
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 30% 100%)",
+            }}
+          />
+          <Icon>
+            <img
+              src="https://kstatic.googleusercontent.com/files/811eae0c24a1c64093bacbc3f717cbda8a6e494d48fa1788017542b8e258050dcf185ced5794936ef634f77d18e3a4e67edde7ebcf725e209bd62f2f489ad033"
+              alt=""
+            />
+          </Icon>
         </Card>
 
         <Card>
-              <img src="https://kstatic.googleusercontent.com/files/384118d36070d9761c18af9f59bb8edf71ef2b43199786d77368ffcdfc0ad28ce4fa59ee9366e0fc446cf8aa2019b59575c7fd1fe5181ad4b10e01a7bbadf01b" alt=""/>
-              <Card_title>Compartilhe pedágios, combustível e conversas</Card_title>
-              <Card_description>Tem um assento vazio no seu carro? Precisa de carona? Divida o trajeto com quem está indo para o mesmo destino.</Card_description>
-              <Button>Baixar</Button>
-              <Card_link>Saiba mais</Card_link>
-              <Icon>
-                  <img src="https://kstatic.googleusercontent.com/files/8db732783ede471cf6c6afce2a3e9f4d61da6122a7fd71f04b745c1fe104473a1fd2dfe43718b78a1dda7c8717c70a1024acf0d62c0cc916ba645f1da50d573a" alt=""/>
-              </Icon>
+          <img
+            src="https://kstatic.googleusercontent.com/files/384118d36070d9761c18af9f59bb8edf71ef2b43199786d77368ffcdfc0ad28ce4fa59ee9366e0fc446cf8aa2019b59575c7fd1fe5181ad4b10e01a7bbadf01b"
+            alt=""
+          />
+          <Card_title>Compartilhe pedágios, combustível e conversas</Card_title>
+          <Card_description>
+            Tem um assento vazio no seu carro? Precisa de carona? Divida o
+            trajeto com quem está indo para o mesmo destino.
+          </Card_description>
+          <Button>Baixar</Button>
+          <Card_link>Saiba mais</Card_link>
+          <Icon>
+            <img
+              src="https://kstatic.googleusercontent.com/files/8db732783ede471cf6c6afce2a3e9f4d61da6122a7fd71f04b745c1fe104473a1fd2dfe43718b78a1dda7c8717c70a1024acf0d62c0cc916ba645f1da50d573a"
+              alt=""
+            />
+          </Icon>
         </Card>
       </Content>
-      
 
-    {/* <Hamburguer_menu>
+      {/* <Hamburguer_menu>
       <div class="hamburger-menu-header">
           <a class="logo" href="/"></a> 
           <div class="menu-icon-close" onclick="toggleMenu(false)"></div>
@@ -96,7 +141,7 @@ function Home() {
       </ul>
       <ul class="hamburger-menu-links">
           <li><a href="/">Suporte</a></li>
-          <li><a href="/">Login</a></li>
+          <li><a onClick={() => props.history.push("/search")}>Login</a></li>
       </ul>
 
     </Hamburguer_menu> */}
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,167 +1,156 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    margin: 0;
-    padding: 0;
-    font-family: 'Boing Regular', sans-serif;
-    font-size: 35px;
-    line-height: 35px;
-    color: #000000;
-    background-color: #FFFFFF;
-    `;
+  margin: 0;
+  padding: 0;
+  font-family: "Boing Regular", sans-serif;
+  font-size: 35px;
+  line-height: 35px;
+  color: #000000;
+  background-color: #ffffff;
+`;
 
 export const Content = styled.div`
-
-    display: grid;
-    grid-gap: 35px;
-    grid-template-columns: 1.5fr 1fr;
-    grid-template-rows: 1.4fr 1fr;
-    margin: 0px 35px
-    `;
+  display: grid;
+  grid-gap: 35px;
+  grid-template-columns: 1.5fr 1fr;
+  grid-template-rows: 1.4fr 1fr;
+  margin: 0px 35px;
+`;
 
 export const Header = styled.header`
-    padding: 25px 0px;
-    width: 100%;
-    z-index: 2;
+  padding: 25px 0px;
+  width: 100%;
+  z-index: 2;
 `;
 
-export const Menu= styled.nav`
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: calc(100%-60px);
-    margin: 0 30px;
-    padding: 0;
+export const Menu = styled.nav`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: calc(100%-60px);
+  margin: 0 30px;
+  padding: 0;
 `;
 
-export const Menu_links= styled.ul`
-    display: flex;
-    align-items: center;
-    list-style: none;
-    font-size: 14px;
-    margin: 0;
-    padding: 0;
-    li{
-        margin-right: 35px;
-        cursor: pointer;
-        font-weight: 700;
-        color: #000;
-    }
-    li:hover{
-        color: #3cf;
-    }
-    &:last-child{
-        flex: 1;
-        justify-content: flex-end;
-    }
-
-`;
-
-
-export const Button_outline= styled.a`
-    text-decoration: none;
-    border-radius: 17px;
-    padding: 7px 18px;
-    border: 2px solid currentColor;
-`;
-
-export const Logo= styled.a`
-    background-image: url("https://www.waze.com/webcms/static/compiled/eab8d17c745136dfb5b5c40fb8b2564a.svg");
-    background-position: center;
-    background-repeat: no-repeat;
-    margin-right: 40px;
-    width: 36px;
-    height: 32px;
-`;
-
-export const Menu_Icon_Open= styled.div`
-    display: none;
-`;
-
-export const Card= styled.div`
-    position: relative;
-    padding: 60px 55px 95px;
-    border-radius: 10px;
-    &:nth-child(1){
-        background: #35CBFC;
-        grid-column: 1 / 2;
-        grid-row: 1 / 2;
-        }
-    &:nth-child(2){
-        padding: 0;
-        border-radius: 0;
-        grid-column: 1 / 2;
-        grid-row: 2 / 3;
-    }
-    &:nth-child(3){
-        background: #69e693;
-        grid-column: 2 / 3;
-        grid-row: 1 / 3;
-    }
-`;
-
-export const Card_title= styled.div`
-    font-size: 54px;
-    line-height: 1.05;
-    margin: 15px 0px;
-`;
-
-export const Card_description= styled.p`
-    font-size: 24px;
-    font-weight: 500;
-    line-height: 1.45;
-    margin: 0px 0px 25px 0px;
-`;
-
-
-export const Button= styled.button`
-    font-size: 20px;
-    font-weight: 700;
-    text-decoration: none;
-    border-radius: 26px;
-    margin-right: 15px;
-    padding: 15px 40px;
+export const Menu_links = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  font-size: 14px;
+  margin: 0;
+  padding: 0;
+  li {
+    margin-right: 35px;
     cursor: pointer;
-    background: #ffffff;
-`;
-
-export const Card_link= styled.a`
-    margin: 0;
-    color: #000;
-    font-size: 20px;
     font-weight: 700;
-    text-decoration: none;
-    padding-bottom: 3px;
-    border-bottom-width: 2px;
-    border-bottom-style: solid;
-    border-bottom-color: #000;
-`;
-
-
-export const Icon= styled.a`
-    margin: 0;
     color: #000;
-    font-size: 20px;
-    font-weight: 700;
-    text-decoration: none;
-    padding-bottom: 3px;
-    border-bottom-width: 2px;
-    border-bottom-style: solid;
-    border-bottom-color: #000;
+  }
+  li:hover {
+    color: #3cf;
+  }
+  &:last-child {
+    flex: 1;
+    justify-content: flex-end;
+  }
 `;
 
-
-export const Hamburguer_menu=styled.div`
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    top: 0;
-    right: 100vw;
-    height: calc(100vh - 40px);
-    width: calc(100vw - 40px);
-    padding: 20px;
-    z-index: 2;
-    background: #3cf;
+export const Button_outline = styled.a`
+  text-decoration: none;
+  border-radius: 17px;
+  padding: 7px 18px;
+  border: 2px solid currentColor;
 `;
 
+export const Logo = styled.a`
+  background-image: url("https://www.waze.com/webcms/static/compiled/eab8d17c745136dfb5b5c40fb8b2564a.svg");
+  background-position: center;
+  background-repeat: no-repeat;
+  margin-right: 40px;
+  width: 36px;
+  height: 32px;
+`;
+
+export const Menu_Icon_Open = styled.div`
+  display: none;
+`;
+
+export const Card = styled.div`
+  position: relative;
+  padding: 60px 55px 95px;
+  border-radius: 10px;
+  &:nth-child(1) {
+    background: #35cbfc;
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+  }
+  &:nth-child(2) {
+    padding: 0;
+    border-radius: 0;
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+  }
+  &:nth-child(3) {
+    background: #69e693;
+    grid-column: 2 / 3;
+    grid-row: 1 / 3;
+  }
+`;
+
+export const Card_title = styled.div`
+  font-size: 54px;
+  line-height: 1.05;
+  margin: 15px 0px;
+`;
+
+export const Card_description = styled.p`
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 1.45;
+  margin: 0px 0px 25px 0px;
+`;
+
+export const Button = styled.button`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: none;
+  border: none;
+  border-radius: 26px;
+  margin-right: 15px;
+  padding: 15px 40px;
+  cursor: pointer;
+  background: #ffffff;
+`;
+
+export const Card_link = styled.a`
+  margin: 0;
+  color: #000;
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: none;
+  padding-bottom: 3px;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: #000;
+`;
+
+export const Icon = styled.a`
+  position: absolute;
+  bottom: -17px;
+  left: 60px;
+  z-index: 1;
+`;
+
+export const Hamburguer_menu = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  right: 100vw;
+  height: calc(100vh - 40px);
+  width: calc(100vw - 40px);
+  padding: 20px;
+  z-index: 2;
+  background: #3cf;
+`;
