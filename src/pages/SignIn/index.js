30aa992f64a756b08063f461dac0a4f18b7b2531
Uriel Guy Sá Teles Oliveira <32525421+urieloliveira@users.noreply.github.com>
2021-04-03 17:16:01 -0300

    Revert "Revert "feat: create base project, folders, routes, install dependenc…"

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+function SignIn() {
+  return (
+    <Container>
+      <h1>SignIn</h1>
+    </Container>
+  );
+}
+
+export default SignIn;
